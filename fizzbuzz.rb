ea3c2d558c068684643c6a4cd2b7b1d4561cd56a
Kenneth Kebaara <kkebaara@yahoo.com>
2021-02-17 03:44:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,19 @@ if num % 3 == 0 && num % 2 == 0
   "FizzBuzz"
 elsif num % 3 == 0
   "Fizz"
-elsif num % 2 == 0
+elsif num % 5 == 0
   "Buzz"
 end
+end
+
+
+def fizzbuzz(int)
+  # Why did this operation have to be first?
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
 end
