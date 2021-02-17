f82f96ff9399fd88cd8ca10faa09712fc1366e34
Kenneth Kebaara <kkebaara@yahoo.com>
2021-02-17 03:26:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,4 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num % 3 == 0  && num % 2 == 0# if the number int is divisible by 3
-    puts "FizzBuzz" # Go fizz
-end
-
 end
