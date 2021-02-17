3b4be70e7cc2f4d47e7b888db833948ebf57939e
Kenneth Kebaara <kkebaara@yahoo.com>
2021-02-17 03:42:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-if num % 3 == 0
+if num % 3 == 0 && num % 2 == 0
+  "FizzBuzz"
+elseif num % 3 == 0
   "Fizz"
-else num % 2 == 0
+elseif num % 2 == 0
   "Buzz"
 end
